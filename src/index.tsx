a4c84e810f3b705acc26f34411b555542669a002
Corey <hamhams86@gmail.com>
2022-08-25 17:27:00 -0700

    initial set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {store} from './redux/store'
+// import {FirebaseAppProvider, AuthCheck} from 'reactfire'
+import {Home} from './components'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <FirebaseAppProvider> */}
+    <Provider store={store}>
+      <Router>
+      <Switch>
+        
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+
+      </Switch>
+      </Router>
+    </Provider>
+    {/* </FirebaseAppProvider> */}
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
